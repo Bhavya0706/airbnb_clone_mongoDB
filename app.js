b9f0bf3b76bdbcf2df16bd38ae56039dbe865656
Bhavya0706 <sutharbhavy77@gmail.com>
2026-06-20 19:57:06 +0530

    Add dotenv configuration to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const parser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
 
 const userrouter = require('./routes/userrouter');
